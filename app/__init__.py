2c611c933c45f909041bad1252dbbfedefdb915c
EnmmTOT <346835954@qq.com>
2021-02-05 22:37:56 +0800

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask#从flask包中导入Flask类
+from config import Config#从config模块导入Config类
+
+
+app = Flask(__name__)#将Flask类的实例 赋值给名为 app 的变量。这个实例成为app包的成员。
+app.config.from_object(Config)
+
+
+
+
+from app import routes#从app包中导入模块routes
